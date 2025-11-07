586ef5fd5e06e723d00127bbe1ac5133cff48976
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2025-11-06 22:18:29 -0300

    feat: Implementa um Hello World em JS

---
+++ learningJS/aula1 /script.js
@@ -0,0 +1,24 @@
+function main() {
+  let username = prompt("Qual o seu nome?");
+
+  if (username == null) {
+    alert("Operação cancelada!");
+    return;
+  }
+  if (username.length === 0) {
+    alert("Você precisa digita um nome para continuar!");
+    main();
+  }
+
+  alert(`Olá ${username.trim()}, seja bem-vindo!`);
+
+  let sabeProgramar = confirm("Você sabe programar em JS?");
+
+  if (sabeProgramar) {
+    alert(`Que ótimo ${username}, espero que você aprenda ainda mais!`);
+  } else {
+    alert(`Não tem problema ${username}, você irá aprender agora!`);
+  }
+}
+
+main();
